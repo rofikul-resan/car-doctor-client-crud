483ac807a7e103563f2af8829f16beefdef0e409
rofikul-resan <resan6231@gmail.com>
2023-05-12 23:44:12 +0600

    file setup done

---
+++ src/layout/HomeLayout.jsx
@@ -0,0 +1,5 @@
+const HomeLayout = () => {
+  return <div>this is HomeLayout page</div>;
+};
+
+export default HomeLayout;
---
+++ src/router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../layout/HomeLayout";
+import Home from "../page/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
